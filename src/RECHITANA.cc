26e0b9c1070238defd4ee660e94edcabc31378c5
carrillo <>
2008-04-23 19:59:27 +0000

    boolean hscp implemented waiting for flat hscp

--- src/RECHITANA.cc
+++ src/RECHITANA.cc
@@ -56,14 +56,18 @@ private:
   virtual void analyze(const edm::Event&, const edm::EventSetup&);
   virtual void endJob() ;
   std::ofstream fileMatrix;
-  int matrixbitTOTAL[4][7];
+  //int matrixbitTOTAL[4][7];
   TFile* theFile;
-  TH2F * matrixHisto;
+  TH1F * matrixHisto;
+  int Maxbx[7];
+  int totalHSCP;
+
 };
 
 
 RECHITANA::RECHITANA(const edm::ParameterSet& iConfig)
 {
+  
 }
 
 
@@ -75,23 +79,23 @@ void RECHITANA::beginJob(const edm::EventSetup&)
 {
   fileMatrix.open("fileMatrix.txt");
   theFile = new TFile("rechitana.root", "RECREATE");
-  matrixHisto = new TH2F("LayersandBX","Histogram 2D Layers and BX",6,0.5,6.5,4,-0.5,3.5);
-
-  for(int lay=0;lay<4;lay++){//looping layers
-    for(int bxs=0;bxs<7;bxs++){//looping bx
-      matrixbitTOTAL[bxs][lay]=0;
-    }
-  }
+  //matrixHisto = new TH1F("LayersandBX","Histogram 2D Layers and BX",6,0.5,6.5,4,-0.5,3.5);
+  matrixHisto = new TH1F("LayersandBX","Histogram 2D Layers and BX",30,-0.5,29.5);
+  totalHSCP=0;
+  
+  //for(int lay=0;lay<4;lay++){//looping layers
+  //  for(int bxs=0;bxs<7;bxs++){//looping bx
+  //    matrixbitTOTAL[bxs][lay]=0;
+  //  }
+  //}
 }
 
 void RECHITANA::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
   edm::Handle<RPCRecHitCollection> rpcHits;
   iEvent.getByLabel("rpcRecHits",rpcHits);
-  
   edm::ESHandle<RPCGeometry> rpcGeo;
   iSetup.get<MuonGeometryRecord>().get(rpcGeo);
-  
   RPCRecHitCollection::const_iterator recIt;
   
   //Identifing in the RecHits-HSPC
@@ -111,7 +115,6 @@ void RECHITANA::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
     
     int BX = (*recIt).BunchX();
     int layer=0;
-       
     int mySt = RPCId.station();
     
     if(RPCId.region()==0){
@@ -130,7 +133,7 @@ void RECHITANA::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
     }
   }
   
-  bool hspc=false;
+  fileMatrix<<"\n";
 
   for(int bxs=bxsitLimit-1;bxs>=0;bxs--){//looping bx
     for(int lay=1;lay<layitLimit;lay++){//looping layers
@@ -138,7 +141,6 @@ void RECHITANA::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
     }
     fileMatrix<<"\n";
   }
-  fileMatrix<<"\n";
 
   //When we count endcaps this should work
   //assert(counter==rpcHits->size());
@@ -146,26 +148,71 @@ void RECHITANA::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
   std::cout<<"Printing matrix Event "<<iEvent.id().event()<<std::endl;
   fileMatrix<<"Printing matrix Event "<<iEvent.id().event()<<std::endl;
 
-  for(int bxs=bxsitLimit-1;bxs>=0;bxs--){//looping bx
-    for(int lay=1;lay<layitLimit;lay++){//looping layers
+  bool hscp = false;
+  int layersHited = 0;
+  std::cout<<"Cleaning array Maxbx"<<std::endl;
+  for(int i=0;i<7;i++)Maxbx[i]=0;
+  std::cout<<"Cleaned"<<std::endl;
+
+  for(int lay=1;lay<7;lay++){//looping layers
+    bool anyhit = false;
+    int maxbx = 0;
+    for(int bxs=bxsitLimit-1;bxs>=0;bxs--){//looping bx
       if(matrixbit[bxs][lay]!=0.){
-	fileMatrix<<"Increasing "<<bxs<<" "<<lay<<" "<<matrixbit[bxs][lay]<<std::endl;
-	matrixHisto->Fill(bxs,lay,matrixbit[bxs][lay]);
+	//fileMatrix<<"Increasing "<<bxs<<" "<<lay<<" "<<matrixbit[bxs][lay]<<std::endl;
+	matrixHisto->Fill(bxs+(lay-1)*5,matrixbit[bxs][lay]);
+	if(matrixbit[bxs][lay]!=0){
+	  anyhit=true;
+	  if(maxbx<bxs)maxbx=bxs;
+	}
       }
     }	
+    if(anyhit)layersHited++;
+    std::cout<<"Writing the array"<<" lay = "<<lay<<" maxbx = "<<maxbx<<std::endl;
+    Maxbx[lay]=maxbx;
   }
+
+  fileMatrix<<"Number Of layers "<<layersHited<<std::endl;
+  for(int i=1;i<7;i++)fileMatrix<<" L"<<i<<" "<<Maxbx[i];
+  fileMatrix<<"\n";
+
+  fileMatrix<<"pendpos ";
+  
+  int pendpos=0;
+  for(int i=1;i<6;i++){
+    if(Maxbx[i]<Maxbx[i+1]){
+      pendpos++;
+    }
+    if(Maxbx[i]>Maxbx[i+1]){
+      pendpos--;
+    }
+    fileMatrix<<" "<<pendpos;
+    
+  }
+  
+  
+  std::cout<<"Number Of layers "<<layersHited<<std::endl;
+  
+  if(layersHited>=3&&pendpos>0)hscp=true;
+  
+  std::cout<<" pendpos= "<<pendpos<<" boolean hscp= "<<hscp<<std::endl;
+  fileMatrix<<" pendpos= "<<pendpos<<" boolean hscp= "<<hscp<<std::endl;
+  
+  if(hscp) totalHSCP++;
+  
   
   //updating total counter
 
-  for(int bxs=bxsitLimit-1;bxs>=0;bxs--){//looping bx
-    for(int lay=1;lay<layitLimit;lay++){//looping layers
-      matrixbitTOTAL[bxs][lay]=matrixbitTOTAL[bxs][lay]+matrixbit[bxs][lay];
-    }	
-  }
+  //for(int bxs=bxsitLimit-1;bxs>=0;bxs--){//looping bx
+  //  for(int lay=1;lay<layitLimit;lay++){//looping layers
+  //    matrixbitTOTAL[bxs][lay]=matrixbitTOTAL[bxs][lay]+matrixbit[bxs][lay];
+  //  }	
+  //}
 }
 
 void RECHITANA::endJob() {
   
+  /*
   for(int bxs=7-1;bxs>=0;bxs--){//looping bx
     for(int lay=1;lay<4;lay++){//looping layers
       fileMatrix<<matrixbitTOTAL[bxs][lay]<<"\t";
@@ -174,6 +221,10 @@ void RECHITANA::endJob() {
   }
   fileMatrix<<"\n";
   
+  */
+  std::cout<<"\n TOTAL HSPCs = "<<totalHSCP<<std::endl;
+  fileMatrix<<"\n TOTAL HSPCs = "<<totalHSCP<<std::endl;
+
   theFile->cd();
   matrixHisto->Write();
   theFile->Close();
