5abc55f9750fa4f7710a5c2ab24d6cdb1a8a1e38
carrillo <>
2009-08-10 14:09:10 +0000

    hvscan generic working for barrel

---
+++ hvscan/dohvscan6P.sh
@@ -0,0 +1,34 @@
+rm GraphAndFit.C
+sed -e "s|-side-|Wm2near|g" -e "s|-N-|101|g" GraphAndFittemplate6Points.C > GraphAndFit.C
+root -b GraphAndFit.C
+rm GraphAndFit.C
+sed -e "s|-side-|Wm2far|g" -e "s|-N-|105|g" GraphAndFittemplate6Points.C > GraphAndFit.C
+root -b GraphAndFit.C
+rm GraphAndFit.C
+sed -e "s|-side-|Wm1near|g" -e "s|-N-|101|g" GraphAndFittemplate6Points.C > GraphAndFit.C
+root -b GraphAndFit.C
+rm GraphAndFit.C
+sed -e "s|-side-|Wm1far|g" -e "s|-N-|105|g" GraphAndFittemplate6Points.C > GraphAndFit.C
+root -b GraphAndFit.C
+rm GraphAndFit.C
+sed -e "s|-side-|W0near|g" -e "s|-N-|101|g" GraphAndFittemplate6Points.C > GraphAndFit.C
+root -b GraphAndFit.C
+rm GraphAndFit.C
+sed -e "s|-side-|W0far|g" -e "s|-N-|105|g" GraphAndFittemplate6Points.C > GraphAndFit.C
+root -b GraphAndFit.C
+rm GraphAndFit.C
+sed -e "s|-side-|W1near|g" -e "s|-N-|101|g" GraphAndFittemplate6Points.C > GraphAndFit.C
+root -b GraphAndFit.C
+rm GraphAndFit.C
+sed -e "s|-side-|W1far|g" -e "s|-N-|105|g" GraphAndFittemplate6Points.C > GraphAndFit.C
+root -b GraphAndFit.C
+rm GraphAndFit.C
+sed -e "s|-side-|W2near|g" -e "s|-N-|101|g" GraphAndFittemplate6Points.C > GraphAndFit.C
+root -b GraphAndFit.C
+rm GraphAndFit.C
+sed -e "s|-side-|W2far|g" -e "s|-N-|105|g" GraphAndFittemplate6Points.C > GraphAndFit.C
+root -b GraphAndFit.C
+source genall.txt
+hadd -f barrel.root effDistro*.root
+hadd -f wp.root wp*.root
+
---
+++ hvscan/splitAllBySide.sh
@@ -0,0 +1,14 @@
+#!/bin/tcsh
+cd -HV1-
+../splitBySide.sh roll*
+cd ../-HV2-
+../splitBySide.sh roll*
+cd ../-HV3-
+../splitBySide.sh roll*
+cd ../-HV4-
+../splitBySide.sh roll*
+cd ../-HV5-
+../splitBySide.sh roll*
+cd ../-HV6-
+../splitBySide.sh roll*
+cd ../
