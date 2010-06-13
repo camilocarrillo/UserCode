2105c423f7268b898cd377bf2cd50f7cf1350782
carrillo <>
2010-06-13 16:19:46 +0000

    first release

---
+++ MuonRPCMonitor/interface/MuonRPCMonitor.h
@@ -0,0 +1,89 @@
+#include <memory>
+
+// user include files
+
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+//
+// class declaration
+//
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "DQMServices/Core/interface/DQMStore.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+
+#include <DataFormats/MuonDetId/interface/RPCDetId.h>
+#include "FWCore/ParameterSet/interface/InputTag.h"
+#include <Geometry/CommonDetUnit/interface/GeomDet.h>
+#include "MagneticField/Engine/interface/MagneticField.h"
+#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
+#include "Geometry/Records/interface/GlobalTrackingGeometryRecord.h"
+#include "Geometry/CommonDetUnit/interface/GeomDet.h"
+#include <DataFormats/GeometrySurface/interface/LocalError.h>
+#include <DataFormats/GeometryVector/interface/LocalPoint.h>
+#include "DataFormats/GeometrySurface/interface/Surface.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrack.h"
+#include "DataFormats/DetId/interface/DetId.h"
+#include "DataFormats/MuonDetId/interface/MuonSubdetId.h"
+#include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
+#include "RecoMuon/TrackingTools/interface/MuonServiceProxy.h"
+#include "RecoMuon/MeasurementDet/interface/MuonDetLayerMeasurements.h"
+#include "TrackingTools/PatternTools/interface/TrajMeasLessEstim.h"
+#include "TrackingTools/GeomPropagators/interface/Propagator.h"
+#include "TrackingTools/PatternTools/interface/MeasurementEstimator.h"
+#include "RecoMuon/Navigation/interface/DirectMuonNavigation.h"
+#include "TrackingTools/TrajectoryState/interface/FreeTrajectoryState.h"
+#include "TrackingTools/PatternTools/interface/TrajectoryMeasurement.h"
+#include "TrackingTools/DetLayers/interface/DetLayer.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticleFwd.h"
+#include "TrackingTools/TrajectoryState/interface/TrajectoryStateOnSurface.h"
+#include "TrackingTools/GeomPropagators/interface/Propagator.h"
+#include "TrackingTools/PatternTools/interface/MeasurementEstimator.h"
+#include "RecoMuon/TransientTrackingRecHit/interface/MuonTransientTrackingRecHit.h"
+#include "TrackingTools/MeasurementDet/interface/TrajectoryMeasurementGroup.h"
+#include <Geometry/Records/interface/MuonGeometryRecord.h>
+#include <Geometry/RPCGeometry/interface/RPCGeometry.h>
+#include <Geometry/RPCGeometry/interface/RPCGeomServ.h>
+#include <Geometry/RPCGeometry/interface/RPCRoll.h> 
+#include "AnalysisDataFormats/SUSYBSMObjects/interface/HSCParticle.h"
+#include <DataFormats/RPCRecHit/interface/RPCRecHit.h>
+#include "DataFormats/TrajectorySeed/interface/TrajectorySeedCollection.h"
+#include "TrackingTools/PatternTools/interface/Trajectory.h"
+#include "TrackingTools/PatternTools/interface/TrajTrackAssociation.h"
+#include "DataFormats/TrackCandidate/interface/TrackCandidate.h" 
+#include "SimDataFormats/TrackingHit/interface/PSimHitContainer.h"
+
+
+class MuonRPCMonitor : public edm::EDAnalyzer {
+   public:
+      explicit MuonRPCMonitor(const edm::ParameterSet&);
+      ~MuonRPCMonitor();
+      edm::ESHandle<RPCGeometry> rpcGeo;
+      std::map<std::string, MonitorElement*> bookDetUnitSeg(RPCDetId & detId);
+      virtual void beginRun(const edm::Run&, const edm::EventSetup&);
+      virtual void analyze(const edm::Event&, const edm::EventSetup&);
+      virtual void endRun(const edm::Run&, const edm::EventSetup&) ;
+      MonitorElement * observedeta;
+      MonitorElement * observedphi;
+      MonitorElement * observedp;
+      MonitorElement * observedNhits;
+      MonitorElement * observedNhitsRPC;
+      MonitorElement * observedTracksPerEvent;
+
+  private:
+      
+      // ----------member data ---------------------------
+
+      std::string  m_trackTag;
+      std::string rootFileName;
+      int MinRPCRecHits;
+      DQMStore * dbe;
+      std::map<std::string, std::map<std::string, MonitorElement*> >  meCollection;
+};
