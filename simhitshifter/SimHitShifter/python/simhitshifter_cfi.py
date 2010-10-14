d36b7709f8e984a6b3d194d4ef87d2b07770abae
carrillo <>
2010-10-14 09:19:27 +0000

    update config

--- simhitshifter/SimHitShifter/python/simhitshifter_cfi.py
+++ simhitshifter/SimHitShifter/python/simhitshifter_cfi.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-theshifter = cms.EDProducer('SimHitShifter'
-
+theshifter = cms.EDProducer("SimHitShifter",
+  ShiftFileName =cms.untracked.string('~/simhits/testFromScratch/CMSSW_3_5_8_patch2/src/simhitshifter/SimHitShifter/Merged_Muon_RawId_Shift.txt')
 )
+
