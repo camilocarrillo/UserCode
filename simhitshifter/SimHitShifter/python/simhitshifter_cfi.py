9a632c0223f37b8890238d8693ede8d7e53b63f4
carrillo <>
2011-06-15 12:41:48 +0000

    2011 conditions

--- simhitshifter/SimHitShifter/python/simhitshifter_cfi.py
+++ simhitshifter/SimHitShifter/python/simhitshifter_cfi.py
@@ -1,8 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-theshifter = cms.EDProducer("SimHitShifter",
-#  ShiftFileName = cms.untracked.string('/afs/cern.ch/user/c/carrillo/simhits/marcelloTEst/CMSSW_3_5_8_patch2/src/simhitshifter/SimHitShifter/goodtest.txt')
-#  ShiftFileName = cms.untracked.string('/afs/cern.ch/user/c/carrillo/simhits/marcelloTEst/CMSSW_3_5_8_patch2/src/simhitshifter/SimHitShifter/noMap.txt')
-   ShiftFileName = cms.untracked.string('/afs/cern.ch/user/c/carrillo/simhits/marcelloTEst/CMSSW_3_5_8_patch2/src/simhitshifter/SimHitShifter/goodMap.txt')
+g4SimHits = cms.EDProducer("SimHitShifter",
+#  ShiftFileName = cms.untracked.string('../data/goodtest.txt')
+#  ShiftFileName = cms.untracked.string('../data/noMap.txt')
+#  ShiftFileName = cms.untracked.string('../data/goodMap.txt')
+   ShiftFileName = cms.untracked.string('../data/2011Map.txt')
 )
 
